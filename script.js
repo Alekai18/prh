3374e8c70cfe1a44b038b6801eb4d2bc85065b2e
Alekai18 <realityjohan567@gmail.com>
2025-03-18 22:00:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").addEventListener("click", function() {
+    let mensaje = document.getElementById("mensaje");
+    mensaje.classList.add("mostrar"); // Agrega la clase para activar la animación
+});
